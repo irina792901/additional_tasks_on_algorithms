e5d6740caf9f0826f4186953d5abce811b32f9c5
Irina_Shipunova <irina_shipunova@list.ru>
2022-08-08 18:01:15 +0700

    add 4 and 5 task INVALID

---
+++ Folder_for_solutions_in_code/task4Easy/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Введение в программирование(семинары)
+Урок 3. Сравниваем разные алгоритмы решения задач.
+Уровень EASY
+Задача 4. С клавиатуры вводится массив из N целых чисел. 
+Определить, есть ли в нем элементы, равные среднему 
+арифметическому среди всех жлементов. 
+Вывести на экран ответ: Да/Нет 
+*/
+using static System.Console;
+int[] array = new int[10]; 
+int size= array.Length;
+int index = 0;
+int sum = 0;
+string answer = "Нет";
+while (index < size)
+{
+    WriteLine("Введите целое число для наполнения массива ");
+    array[index] = Convert.ToInt32(ReadLine());
+    sum += array[index];
+    index++;
+}
+int arg = sum/size;
+index = 0;
+while (index<size)
+{
+    if (array[index]==arg) answer = "Да";
+    index++;
+} 
+WriteLine(answer);
+
---
+++ Folder_for_solutions_in_code/task5Easy/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Введение в программирование(семинары)
+Урок 3. Сравниваем разные алгоритмы решения задач.
+Уровень EASY
+Задача 5. С клавиатуры вводится массив из N целх чисел. 
+Найти среднее арифметическое (СА) и среднее геометрическое
+(СГ) среди всех элементов. Затем найти сумму элементов 
+массива меньших СА, но больших СГ.
+*/
+using static System.Console;
+int[] array = new int[10]; 
+int size= array.Length;
+int index = 0;
+int sum = 0;
+int mult = 1;
+while (index < size)
+{
+    WriteLine("Введите целое число для наполнения массива ");
+    array[index] = Convert.ToInt32(ReadLine());
+    sum += array[index];
+    mult*=array[index];
+    index++;
+}
+int Sar = sum/size;
+double Sgeo = mult,; // TO
+index = 0;
+int sumEl = 0;
+while (index<size)
+{
+    if (array[index]>Sgeo && array[index]<Sar) sumEl+=array[index];
+    index++;
+} 
+WriteLine(sumEl);
